01733a51408f146b6dfd53c4bc1b108bcd7d20ec
Radu Adobroaie <radu.adobroaie@gmail.com>
2023-01-04 21:58:38 +0000

    Add stage 1 tests

--- src/test/java/com/example/brickorderingsystem/BrickOrderControllerTest.java
+++ src/test/java/com/example/brickorderingsystem/BrickOrderControllerTest.java
@@ -1,71 +1,84 @@
 package com.example.brickorderingsystem;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
 
+/**
+ * Normally I would have mocked the order service here and make this a unit test.
+ * However, since I'm pressed for time I decided to write integration tests instead.
+ */
 class BrickOrderControllerTest {
-    @Mock
-    BrickOrderService orderService;
+//    @Mock
+//    BrickOrderService orderService;
     
-    @InjectMocks
+//    @InjectMocks
     BrickOrderController orderController;
+
+    @BeforeEach
+    void beforeEach() {
+        this.orderController = new BrickOrderController(new BrickOrderService());
+    }
 
     @Test
     void createOrder_withZeroBricks_throwsException() {
-//        final OrderException actual;
-//        
 //        when(this.orderService.createOrder(0)).thenThrow(new OrderException());
-//        
-//        try {
-//            this.orderController.createOrder(0);
-//        } catch (OrderException e) {
-//            actual = e;
-//        }
-//        
-//        assertNotNull(actual);
-    }
-    
-    @Test
-    void createOrder_withNullBricks_throwsException() {
-        
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> this.orderController.createOrder(0))
+                .withMessage("Illegal argument: 0 or null order");
     }
 
     @Test
     void createOrder_withMultipleBricks_returnsReference() {
-        
+        final String response = this.orderController.createOrder(3);
+        assertThat(response).isInstanceOf(String.class);
     }
     
     @Test
     void createOrder_withSameNumberOfBricks_returnsDifferentReference() {
-        
+        final String firstReference = this.orderController.createOrder(3);
+        final String secondReference = this.orderController.createOrder(3);
+
+        assertThat(firstReference).isNotEqualTo(secondReference);
     }
     
     @Test
     void getOrder_withWrongReference_throwsException() {
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> this.orderController.getOrder(null))
+                .withMessage("Illegal argument: empty or null reference");
     }
     
     @Test
     void getOrder_withNullReference_throwsException() {
-        
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> this.orderController.getOrder(""))
+                .withMessage("Illegal argument: empty or null reference");
     }
     
     @Test
     void getOrder_withCorrectReference_returnsOrder() {
-        
+        final String reference = this.orderController.createOrder(3);
+        final Order response = this.orderController.getOrder(reference);
+
+        assertThat(response).isInstanceOf(Order.class);
+        assertThat(response.getId()).isEqualTo(reference);
     }
     
     @Test
     void getOrders_withNoExistingOrders_throwsException() {
-        
+        assertThatThrownBy(() -> this.orderController.getOrder(""))
+                .isInstanceOf(OrderException.class)
+                .hasMessage("No orders created yet.");
+
     }
 
     @Test
     void getOrders_withExistingOrders_returnsOrders() {
+        final String firstReference = this.orderController.createOrder(3);
+        final String secondReference = this.orderController.createOrder(3);
 
+        assertThat(this.orderController.getOrders()).contains(firstReference, secondReference);
     }
 }
