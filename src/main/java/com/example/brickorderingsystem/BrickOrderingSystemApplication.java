21de33fea3c565b410478c2a32c3b318c86d6b43
Radu Adobroaie <radu.adobroaie@gmail.com>
2023-01-04 18:36:49 +0000

    Initial commit

---
+++ src/main/java/com/example/brickorderingsystem/BrickOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.brickorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brickorderingsystem/BrickOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brickorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
